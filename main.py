387a53f8bac4949f3e6cde8e4b2db0aca57739f7
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-03-20 12:14:36 +0300

    Adding blocks to work with Google Sheet file

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 #This file will need to use the DataManager,FlightSearch, FlightData,
 # NotificationManager classes to achieve the program requirements.
 
-print("begin again and add new items")
+"""
+APIs Required
+Google Sheet Data Management - https://sheety.co/
+
+Kiwi Partners Flight Search API (Free Signup, Requires Credit Card Details) - https://partners.kiwi.com/
+
+Tequila Flight Search API Documentation - https://tequila.kiwi.com/portal/docs/tequila_api
+
+Twilio SMS API - https://www.twilio.com/docs/sms
+"""
+
+# Google Sheet with information about flights
+url = "https://api.sheety.co/afd998d332f85c1909b2035741e296da/flightDeals/prices"
+
+# with

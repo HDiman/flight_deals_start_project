f2d5b9c35aa689bf72491fba0f78491e75564b9c
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-03-20 11:56:36 +0300

    Adding google sheet file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #This file will need to use the DataManager,FlightSearch, FlightData,
 # NotificationManager classes to achieve the program requirements.
 
-print("begin again")
+print("begin again and add new items")

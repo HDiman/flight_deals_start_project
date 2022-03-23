4c3256db4ce775ee1dbd4deb0339ad21de0c0d24
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-03-23 08:45:34 +0300

    Added one new city

--- data.py
+++ data.py
@@ -1,16 +1,21 @@
 # Dictionary instead of Google Sheet
 rows = {
     "1": {
+        "city": "Moscow",
+        "iataCode": "MOW",
+        "lowest_price": "2000",
+    },
+    "2": {
         "city": "Kaliningrad",
         "iataCode": "KGD",
         "lowest_price": "2000",
     },
-    "2": {
+    "3": {
         "city": "Saratov",
         "iataCode": "GSV",
         "lowest_price": "2000",
     },
-    "3": {
+    "4": {
         "city": "Saint-Petersburg",
         "iataCode": "LED",
         "lowest_price": "2000",

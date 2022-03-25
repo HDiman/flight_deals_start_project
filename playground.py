bb1ed3a065ff85fc4d1491ef9ea868e85a8b0017
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-03-26 00:00:28 +0300

    Asyncoi

--- playground.py
+++ playground.py
@@ -56,3 +56,18 @@ Twilio SMS API - https://www.twilio.com/docs/sms
 
 # ---------------------------------------------------------------
 
+import asyncio
+import time
+
+async def async_func():
+    print('Запуск ...')
+    await asyncio.sleep(1)
+    print('... Готово!')
+
+
+async def main():
+    # async_func()  # этот код ничего не вернет
+    await async_func()
+
+
+asyncio.run(main())

91e577c8c0e10b2c3169afe054ff2736da2ac9e0
Dmitry Sannikov <HDiman@users.noreply.github.com>
2022-03-27 14:17:53 +0300

    Adding both direction function to main

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from flight_search import *
 
 ticket_price = FlightSearch()
 
-ticket_price.a_city = "KGD"
-ticket_price.b_city = "VKO"
+def one_way(start, end):
+    ticket_price.a_city = start
+    ticket_price.b_city = end
+    ticket_price.search_flight(365)
 
-ticket_price.search_flight(200)
+    print(ticket_price.lowest_price)
+    print(f"{len(ticket_price.lowest_price)} days")
+    print(f"Lowest price is: {min(ticket_price.lowest_price)} rub.")
 
-print(ticket_price.lowest_price)
-print(f"{len(ticket_price.lowest_price)} days")
-print(f"Lowest price is: {min(ticket_price.lowest_price)} rub.")
+moscow = "MOW"
+moscow_vnukovo = "VKO"
+moscow_sheremetyevo = "SVO"
+moscow_domodedovo = "DME"
+kaliningrad = "KGD"
 
+one_way(start=moscow, end=kaliningrad) # trip to kaliningrad
+one_way(start=kaliningrad, end=moscow_vnukovo) # trip to moscow
